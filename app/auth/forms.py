629011852370c86e81ef4e4ea9d6d9bfcdd0dcf3
brenocrsj <brenocarlos200601@gmail.com>
2026-03-02 15:12:47 +0000

    Fix login form + move static to app/static + update templates/layout

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Email, Length, EqualTo
 
 class LoginForm(FlaskForm):
     email = StringField("E-mail", validators=[DataRequired(), Email()])
     password = PasswordField("Senha", validators=[DataRequired(), Length(min=4)])
+    remember_me = BooleanField("Lembrar-me")   # ✅ ADICIONADO
     submit = SubmitField("Entrar")
 
 class RegisterForm(FlaskForm):
